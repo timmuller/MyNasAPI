a6a5bd4973e028ab8c93130cf4b2049c10ea2e2d
Tim Muller <tim@byte.nl>
2014-05-13 12:22:18 +0200

    fix missing s of %s

--- control/movie_manager.py
+++ control/movie_manager.py
@@ -7,7 +7,7 @@ def list_movies(location):
     for file_location in os.listdir(location):
         full_file_location = os.path.join(location, file_location)
         if is_movie_type(full_file_location):
-            logging.debug("% is a movie, save for future process" % full_file_location)
+            logging.debug("%s is a movie, save for future process" % full_file_location)
             mylocations += [full_file_location]
     return mylocations
 
